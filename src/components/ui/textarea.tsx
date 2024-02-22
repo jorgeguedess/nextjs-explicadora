c1660ae01cb911127305d6ef2223f0f6ac4fa0f8
jorgeguedess <jorge_guedes00@hotmail.com>
2024-02-22 17:55:53 -0300

    feat: add Input and Textare components

---
+++ src/components/ui/textarea.tsx
@@ -0,0 +1,24 @@
+import * as React from "react";
+
+import { cn } from "@/lib/utils";
+
+export interface TextareaProps
+  extends React.TextareaHTMLAttributes<HTMLTextAreaElement> {}
+
+const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(
+  ({ className, ...props }, ref) => {
+    return (
+      <textarea
+        className={cn(
+          "focus-visible:ring-ring flex min-h-[80px] w-full resize-none rounded-md border border-accent bg-accent px-3 py-4 text-sm shadow-sm placeholder:text-secondary focus-visible:outline-none focus-visible:ring-2 disabled:cursor-not-allowed disabled:opacity-50",
+          className,
+        )}
+        ref={ref}
+        {...props}
+      />
+    );
+  },
+);
+Textarea.displayName = "Textarea";
+
+export { Textarea };
