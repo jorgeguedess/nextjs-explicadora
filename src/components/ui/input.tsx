49f37799539ed5d3c86101f4c19999609cb90a39
jorgeguedess <jorge_guedes00@hotmail.com>
2024-02-23 23:31:03 -0300

    styles: adjusting button disabled styles and add new variable input

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -4,15 +4,15 @@ import { cn } from "@/lib/utils";
 export interface InputProps
   extends React.InputHTMLAttributes<HTMLInputElement> {}
 
+export const inputClass =
+  "focus-visible:ring-ring flex h-12 w-full rounded-md border border-accent bg-accent px-3 py-4 text-sm shadow-sm transition-colors placeholder:text-secondary focus-visible:outline-none focus-visible:ring-2 disabled:cursor-not-allowed disabled:opacity-50";
+
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
   ({ className, type, ...props }, ref) => {
     return (
       <input
         type={type}
-        className={cn(
-          "focus-visible:ring-ring flex h-12 w-full rounded-md border border-accent bg-accent px-3 py-4 text-sm shadow-sm transition-colors placeholder:text-secondary focus-visible:outline-none focus-visible:ring-2 disabled:cursor-not-allowed disabled:opacity-50",
-          className,
-        )}
+        className={cn(inputClass, className)}
         ref={ref}
         {...props}
       />
