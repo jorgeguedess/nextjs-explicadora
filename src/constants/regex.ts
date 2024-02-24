09e137ebfab647c0392fdd726ab449a4c5535879
jorgeguedess <jorge_guedes00@hotmail.com>
2024-02-23 23:33:58 -0300

    reafctor: adjusting regex in phone

--- src/constants/regex.ts
+++ src/constants/regex.ts
@@ -1,8 +1,1 @@
-export const phoneRegex = new RegExp(
-  /\(?([0-9]{3})\)?([ .-]?)([0-9]{3})\2([0-9]{4})/,
-);
-export const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-
-export const isEmailOrPhone = (value: string): boolean => {
-  return emailRegex.test(value) || phoneRegex.test(value);
-};
+export const phoneRegex = /^\(\d{2}\) \d{5}-\d{4}$/;
